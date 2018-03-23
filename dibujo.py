5c4f8a4c7f0073c9b384583e7d069b58927480c7
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2018-03-23 23:18:51 +0100

    Update dibujo.py

--- dibujo.py
+++ dibujo.py
@@ -1,7 +1,4 @@
 from turtle import Turtle
-import time
-from VALID import OKI
-
 t=Turtle()
 t.screen.bgcolor("black")
 colors=["red","yellow","purple"]
@@ -11,7 +8,6 @@ for x in range(100):
     t.circle(x)#"x" va tomando los valores del rango, por lo que el circulo se irá incrementando
     t.color(colors[x%3])
     t.left(60)
-    time.sleep(0.00003)
-t.screen.exitonclick()
-t.screen.mainloop()
+#t.screen.exitonclick()
+#t.screen.mainloop()
     
