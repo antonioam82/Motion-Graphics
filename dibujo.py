ee87f809f142a50e0ef968b7e758cb23c9008ba9
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2018-03-30 13:05:07 +0200

    Update dibujo.py

--- dibujo.py
+++ dibujo.py
@@ -1,13 +1,61 @@
 from turtle import Turtle
-t=Turtle()
-t.screen.bgcolor("black")
-colors=["red","yellow","purple"]
-#t.screen.tracer(0,0)
+from VALID import OKI, ns, opt
+import subprocess
 
-for x in range(100):
-    t.circle(x)#"x" va tomando los valores del rango, por lo que el circulo se irá incrementando
-    t.color(colors[x%3])
-    t.left(60)
-#t.screen.exitonclick()
-#t.screen.mainloop()
-    
+while True:
+    t=Turtle()
+    print("Escoja dibujo.")
+    print("A)Flor")
+    print("B)Hexagono com")
+    print("C)Cuadrado")
+    print("D)Hexagono simp")
+    print("E)Cuadrado spiral")
+    print("F)Raizes")
+    op=opt(input("Introduzca su opción: "),["A","B","C","D","E"])
+
+    if op==("A"):
+        t.screen.bgcolor("black")
+        colors=["red","yellow","purple"]
+        for x in range(100):
+            t.circle(x)
+            t.color(colors[x%3])
+            t.left(60)
+    elif op==("B"):
+        t.screen.bgcolor("white")
+        colors=["red","purple","blue","green","orange"]
+        for x in range(150):
+            t.color(colors[x%5])
+            t.width(x/10+1)
+            t.fd(x)
+            t.left(59)
+    elif op==("C"):
+        t.screen.bgcolor("black")
+        colors=["blue","purple","red","yellow"]
+        for x in range(300):
+            t.color(colors[x%4])
+            t.fd(x)
+            t.left(90)
+    elif op==("D"):
+        t.screen.bgcolor("black")
+        colors=["blue","purple","red","yellow","orange","brown"]
+        for x in range(300):
+            t.color(colors[x%6])
+            t.fd(x)
+            t.left(59)
+    elif op==("E"):
+        t.screen.bgcolor("white")
+        colors=["red","green","blue","orange"]
+        for x in range(200):
+            t.color(colors[x%4])
+            t.fd(x)
+            t.left(91)
+            
+    t.hideturtle()
+    conti=ns(input("¿Continuar?: "))
+    if conti==("s"):
+        t.reset()
+        t.hideturtle()
+        subprocess.call(["cmd.exe","/C","cls"])
+    else:
+        break
+            
